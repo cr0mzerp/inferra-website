ec00437df85d4b8f4c189b338bf51ebd37ec7e87
cr0mzerp <sahinbatin722@gmail.com>
2026-06-19 20:08:19 +0400

    fix: remove outputFileTracingRoot for Vercel compat

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require("path")
-
 const nextConfig = {
   transpilePackages: ["three"],
-  outputFileTracingRoot: path.join(__dirname, "../.."),
 }
 
 module.exports = nextConfig
